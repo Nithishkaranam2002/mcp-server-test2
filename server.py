e95e3c5fc145d2f4c967a823a86502f64067664f
Nithish Karanam <nithishkaranam15@gmail.com>
2025-08-03 11:38:19 -0500

    server.py

--- server.py
+++ server.py
@@ -8,4 +8,5 @@ def greeting(name: str) -> str:
     return f"Hi {name}"
 
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http", host="0.0.0.0", port=8000)
+    mcp.run(transport="streamable-http", uvicorn_kwargs={"host": "0.0.0.0", "port": 8000})
+
